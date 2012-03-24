5973e0d73c4a17131221726d61d2c208e73ca25b
Alex Morega <alex@grep.ro>
2012-03-24 16:45:26 +0200

    generate one location for each coord

--- parse_metropotam.py
+++ parse_metropotam.py
@@ -4,13 +4,13 @@ import lxml.etree
 def parse_metropotam_xml(xml_file):
     doc = lxml.etree.parse(xml_file)
     for location in doc.iter('location'):
-        out = {'name': location.attrib['name'], 'coord': []}
         for coord in location.iter('coord'):
-            out['coord'].append({
+            yield {
+                'name': location.attrib['name'],
                 'lat': float(coord.attrib['lat']),
-                'lng': float(coord.attrib['lng']),
-            })
-        yield out
+                'lon': float(coord.attrib['lng']),
+                'type': location.attrib['type'],
+            }
 
 
 def main():
